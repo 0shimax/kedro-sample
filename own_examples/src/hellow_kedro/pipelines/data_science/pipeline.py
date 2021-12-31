0742086bb9778eff389eedae1afea273f343d154
0shimax <shin.keeper@gmail.com>
2021-12-31 13:40:38 +0900

    add

--- own_examples/src/hellow_kedro/pipelines/data_science/pipeline.py
+++ own_examples/src/hellow_kedro/pipelines/data_science/pipeline.py
@@ -1,7 +1,31 @@
 from kedro.pipeline import Pipeline, node
 
-from .nodes import # evaluate_model, split_data, train_model
+from .nodes import split_data, train_model, evaluate_model
 
 
 def create_pipeline(**kwargs):
-    return Pipeline([node()])
+    return Pipeline(
+        [
+            node(
+                func=split_data,
+                inputs=["model_input_table", "parameters"],
+                outputs=["X_train", "X_test", "y_train", "y_test"],
+                name="spliet_data_node",
+                tags=["ds"],
+            ),
+            node(
+                func=train_model,
+                inputs=["X_train", "y_train"],
+                outputs="regressor",
+                name="train_model_node",
+                tags=["ds", "fit"],
+            ),
+            node(
+                func=evaluate_model,
+                inputs=["regressor", "X_test", "y_test"],
+                outputs=None,
+                name="evaluate_model_node",
+                tags=["ds", "eval"],
+            ),
+        ]
+    )
